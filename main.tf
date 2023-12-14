c9e3139ed00dd7c19231820542d2335566d90ef7
viswanath11 <40738371+viswanath11@users.noreply.github.com>
2023-12-14 08:11:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=2.0"
-    }
-  }
-}
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "East US"
-}
+
